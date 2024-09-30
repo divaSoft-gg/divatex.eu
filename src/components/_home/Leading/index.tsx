b5686798e5412720e2b319855bb4bd708dabf667
aymkh <aymen4real@gmail.com>
2024-09-30 14:47:57 +0100

    fix: console log

--- src/components/_home/Leading/index.tsx
+++ src/components/_home/Leading/index.tsx
@@ -9,20 +9,16 @@ export default function Leading() {
     const features: IFeature[] = LEADING_ARRAY;
 
     return (
-        <div className="w-full py-16 bg-white text-black" id="DivaTexHero">
+        <div className="w-full py-16 text-black bg-white" id="DivaTexHero">
             <CenteredLayout>
                 <h1 className="text-5xl text-center">Votre <strong>partenaire technologique</strong> innovant</h1>
                 <p className="text-xl text-center text-[#191919] max-w-[800px] mx-auto mt-8">
                     Découvrez comment DivaTex propulse vos opérations à un niveau supérieur avec des solutions taillées sur mesure pour le secteur textile.
                 </p>
 
-                <div className="featuresGrid my-16">
+                <div className="my-16 featuresGrid">
                     {
-                        features.map((feature: IFeature, index: number) => {
-                            console.log(feature);
-
-                            return <FeatureCard key={index} data={feature} />
-                        }
+                        features.map((feature: IFeature, index: number) => <FeatureCard key={index} data={feature} />
                         )
                     }
                 </div>
