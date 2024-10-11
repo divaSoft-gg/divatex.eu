1b9be5d97e85b3f2070b7d9c19dc4db765ecb1da
hellcat29A <bhiri22@gmail.com>
2024-10-11 16:44:16 +0100

    fix:make-inputs-style-inline

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -4,9 +4,13 @@ export default function HeroSection({ data }: { data: IHeroConfig }) {
 
     return (
         <section
-            className={`w-full min-h-[${data.maxHeight}] bg-[url('${data.coverPath}')] ${data.height && `h-[${data.height}]`}  bg-cover flex justify-center items-center`}
+            className={`w-full   bg-cover flex justify-center items-center`}
+            style={{
+                backgroundImage: `url('${data.coverPath}')`,
+                height: data.height ? data.height : 'inherit',
+                minHeight: data.maxHeight
 
-        >
+            }}        >
             <div className="flex items-center justify-center w-full h-full text-center text-white">
                 <div className="font-primary">
                     {data.renderContent()}
