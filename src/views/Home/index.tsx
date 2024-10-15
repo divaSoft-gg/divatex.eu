0756f562784e3fd46011c70ae6bac0de0b932e30
hellcat29A <bhiri22@gmail.com>
2024-10-15 12:46:41 +0100

    feat:fix-text-size-for-mobile

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -7,26 +7,28 @@ import { Helmet } from "react-helmet-async";
 import Stats from "../../components/_home/Stats";
 import { Button, Link } from "@nextui-org/react";
 import { scrollToTop } from "../../common/utils";
+import { useMediaQuery } from "react-responsive";
 
 export default function Home() {
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
 
     const leadingFeatureArray: IFeature[] = LEADING_ARRAY;
     const heroSectionParams: IHeroConfig = {
         coverPath: "/images/header_background.webp",
-        maxHeight: '900px',
-
+        maxHeight: isMobile ? '400px' : '600px',
         renderContent: () => {
             return (
                 <>
-                    <p className="text-2xl font-medium text-[#0E5EFF]">Solutions dédiées au secteur du textile</p>
-                    <p className="font-light text-7xl leading-snug max-w-[900px] mt-4 mb-8">
+                    <p className="lg:text-2xl text-lg font-medium text-[#0E5EFF]">Solutions dédiées au secteur du textile</p>
+                    <p className="font-light lg:text-7xl text-4xl  leading-snug max-w-[900px] mt-4 mb-8">
                         Augmentez vos rendements et maîtrisez votre rentabilité
                     </p>
 
                     <div className="flex flex-col items-center gap-4 my-16 text-sm font-normal">
-                        <p className="leading-3">Visez l’excellence opérationnelle grâce à notre ERP métier conçu pour amplifier vos</p>
-                        <p className="leading-3">performances et répondre précisément aux besoins de votre secteur. efficacité. Chaque mise en place </p>
-                        <p className="leading-3">est accompagnée d’une augmentation minimale de 10% du CA et de 20% de la rentabilité.</p>
+                        <p className="lg:leading-3">Visez l’excellence opérationnelle grâce à notre ERP métier conçu pour amplifier vos</p>
+                        <p className="lg:leading-3">performances et répondre précisément aux besoins de votre secteur. efficacité. Chaque mise en place </p>
+                        <p className="lg:leading-3">est accompagnée d’une augmentation minimale de 10% du CA et de 20% de la rentabilité.</p>
                     </div>
 
                     <div className="flex flex-row justify-center gap-8 actionButtons">
