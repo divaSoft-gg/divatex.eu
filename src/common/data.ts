a55051c117d0b828fbd55ffbf1e1b70ae7136735
hellcat29A <bhiri22@gmail.com>
2024-11-11 17:23:48 +0100

    feat:add-hero-section

---
+++ src/common/data.ts
@@ -0,0 +1,8 @@
+export const swiperImages: { src: string; alt: string }[] = [
+  { src: "images/hero-section/1.png", alt: "slide 1" },
+  { src: "images/hero-section/1.png", alt: "slide 2" },
+  { src: "images/hero-section/1.png", alt: "slide 3" },
+  { src: "images/hero-section/1.png", alt: "slide 4" },
+  { src: "images/hero-section/1.png", alt: "slide 5" },
+  { src: "images/hero-section/1.png", alt: "slide 6" },
+];
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,26 +1,59 @@
-import { IHeroConfig } from "../../common/types";
+import { Button } from "@nextui-org/react";
+import Navbar from "../partials/navbar";
+import CentredLayout from "../ui/centredLayout";
 
-export default function HeroSection({ data }: { data: IHeroConfig }) {
+import Slider from "react-slick";
+import { swiperImages } from "../../common/data";
+import { useTranslation } from "react-i18next";
 
-
-    return (
-        <section
-            className={`px-10 py-5 flex justify-center items-center  bg-cover bg-center `}
-            style={{
-                backgroundImage: `url('${data.coverPath}')`,
-                backgroundRepeat: 'no-repeat',
-
-                height: data.height ? data.height : 'inherit',
-                minHeight: data.maxHeight,
-
-            }}
-        >
-            <div className="flex items-center justify-center text-white">
-                <div className="font-primary">
-                    {data.renderContent()}
-                </div>
+export default function HeroSection() {
+  const { t } = useTranslation();
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 5000,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+  };
+  return (
+    <section id="hero-section">
+      <CentredLayout>
+        <div className="flex flex-row gap-12">
+          <div className="flex flex-col gap-3">
+            <Navbar />
+            <div className="flex flex-col justify-between h-full">
+              <h1 className="text-6xl font-bold">{t("heroSection.title")}</h1>
+              <p className="text-sm text-gray-800 dark:text-white">
+                {t("heroSection.paragraph")}
+              </p>
             </div>
+            <div className="flex items-center gap-6">
+              <Button> {t("heroSection.moreButton")}</Button>
+              <Button variant="bordered"> {t("heroSection.demoButton")}</Button>
+            </div>
+          </div>
 
-        </section>
-    );
+          <div className="max-w-[500px]">
+            <Slider {...settings}>
+              {swiperImages.map(
+                (element: { src: string; alt: string }, index: number) => (
+                  <div key={index}>
+                    <img
+                      className="shadow-md rounded-2xl"
+                      src={element.src}
+                      alt={element.alt}
+                    />
+                  </div>
+                )
+              )}
+            </Slider>
+          </div>
+        </div>
+      </CentredLayout>
+    </section>
+  );
 }
