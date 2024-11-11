396d8bb7976e656ec50fba977b1c6cdf15b0844c
hellcat29A <bhiri22@gmail.com>
2024-11-11 17:52:49 +0100

    fix:delete-old-project

--- README.md
+++
@@ -1,8 +0,0 @@
-# DivaTEX OFFICIAL WEBSITE
-
-The official website for the Divatex.
-
-Built using
-- Reactjs
-- NextUI
-- TailwindCss
--- src/views/Demo/index.tsx
+++
@@ -1,9 +0,0 @@
-import { InlineWidget } from 'react-calendly'
-
-export default function Demo() {
-    return (
-        <div className='my-12'>
-            <InlineWidget url="https://calendly.com/rochdi-ktir" styles={{ height: 700 }} />
-        </div>
-    )
-}
