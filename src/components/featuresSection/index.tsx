9326ee4d7865e1895756d8ac2755042f810c1d04
hellcat29A <bhiri22@gmail.com>
2024-11-12 17:09:56 +0100

    fix:use-grid-instead-of-flex

--- src/components/featuresSection/index.tsx
+++ src/components/featuresSection/index.tsx
@@ -2,12 +2,14 @@ import { Button, Image } from "@nextui-org/react";
 import CentredLayout from "../ui/centredLayout";
 import { useTranslation } from "react-i18next";
 import { featuresProps } from "../../common/types";
-import { cn } from "../../common/utils";
 
 export default function FeaturesSection() {
   const { t } = useTranslation();
 
-  const features: featuresProps[] = t("featuresSection.features", {
+  const featuresCol1: featuresProps[] = t("featuresSection.featuresCol1", {
+    returnObjects: true,
+  }) as featuresProps[];
+  const featuresCol2: featuresProps[] = t("featuresSection.featuresCol2", {
     returnObjects: true,
   }) as featuresProps[];
 
@@ -26,41 +28,43 @@ export default function FeaturesSection() {
               Voir Plus
             </Button>
           </div>
-
-          {features
-            .reduce<featuresProps[][]>((acc, _, i) => {
-              console.log(acc);
-              if (i % 2 === 0) acc.push(features.slice(i, i + 2));
-              return acc;
-            }, [])
-            .map((row: featuresProps[], rowIndex: number) => (
-              <div key={rowIndex} className="flex flex-row w-full gap-6 h-fit ">
-                {row.map((element: featuresProps, index: number) => (
-                  <div key={index} className={cn("", element.cardWidthClass)}>
-                    <div className="min-h-[33pc] p-8 bg-gray-200 rounded-xl dark:bg-[#161616]">
-                      <div className="flex flex-col gap-8">
-                        <div className="flex flex-row items-center justify-center gap-4">
-                          <img
-                            src={element.icon}
-                            width={25}
-                            className="dark:invert"
-                          />
-                          <h1 className="self-center text-4xl font-medium text-black dark:text-white">
-                            {element.title}
-                          </h1>
-                        </div>
-                        <small>{element.description}</small>
-                        <div className="flex items-center justify-center">
-                          <Image src={element.imagePreview} width={300} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
+          <div className="grid gap-6 sharedGrid">
+            {featuresCol1.map((element: featuresProps, index: number) => (
+              <FeaturesCard element={element} index={index} />
             ))}
+          </div>
+          <div className="grid gap-6 secondGrid">
+            {featuresCol2.map((element: featuresProps, index: number) => (
+              <FeaturesCard element={element} index={index} />
+            ))}
+          </div>
         </div>
       </CentredLayout>
     </section>
+  );
+}
+
+function FeaturesCard({
+  element,
+  index,
+}: Readonly<{ element: featuresProps; index: number }>) {
+  return (
+    <div
+      key={index}
+      className="min-h-[33pc] p-8 bg-gray-200 rounded-xl dark:bg-[#161616]"
+    >
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-row items-center justify-center gap-4">
+          <img src={element.icon} width={25} className="dark:invert" />
+          <h1 className="self-center text-4xl font-medium text-black dark:text-white">
+            {element.title}
+          </h1>
+        </div>
+        <small>{element.description}</small>
+        <div className="flex items-center justify-center">
+          <Image src={element.imagePreview} width={300} />
+        </div>
+      </div>
+    </div>
   );
 }
