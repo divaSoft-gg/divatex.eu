01c80c1f36a29973e03509a5f3c21ecf9d1e5311
hellcat29A <bhiri22@gmail.com>
2024-11-14 17:12:20 +0100

    fix:change-text-size-add-margin-remove-shadow

--- src/views/Demo/index.tsx
+++ src/views/Demo/index.tsx
@@ -13,19 +13,19 @@ export default function DemoView() {
     <section id="contact-us">
       <CentredLayout>
         <div className="flex flex-col gap-12 my-10">
-          <div className="flex flex-row gap-6 bg-[url('/images/cover.jpg')] bg-cover bg-center rounded-2xl  relative shadow-2xl ">
+          <div className="flex flex-row gap-6 bg-[url('/images/cover.jpg')] bg-cover bg-center rounded-2xl  relative shadow-md">
             <div className="absolute inset-0 z-0 bg-black opacity-50 rounded-2xl"></div>
 
             <div className="z-10 flex flex-col justify-center w-2/3 gap-6 p-8">
-              <h1 className="text-5xl text-white">{t("contactUs.title")}</h1>
+              <h1 className="text-5xl text-white">{t("demo.sectionTitle")}</h1>
               <p className="text-base text-white">
                 {t("contactUs.description")}
               </p>
-              <p className="text-white"> {t("contactUs.locationQuestion")}</p>
+              <p className="text-white"> {t("demo.stepsTitle")}</p>
               <ol className="space-y-2 text-white list-decimal">
                 {steps.map((element: demoStepsProps, index: number) => (
                   <li key={index}>
-                    <h1>
+                    <h1 className="text-sm">
                       <strong>{element.title} :</strong>
                       {element.tooltip}
                     </h1>
@@ -33,7 +33,7 @@ export default function DemoView() {
                 ))}
               </ol>
             </div>
-            <div className="absolute w-1/3 h-full bg-white shadow-2xl right-5 top-10 ">
+            <div className="absolute w-1/3 h-full bg-white shadow-md right-5 top-10 ">
               <InlineWidget
                 url="https://calendly.com/rochdi-ktir"
                 styles={{
@@ -42,7 +42,7 @@ export default function DemoView() {
               />
             </div>
           </div>
-          <div className="flex flex-col w-full gap-2 ">
+          <div className="flex flex-col w-full gap-2 mt-8">
             {[0, 1].map((element) => (
               <AutoplayCarousel
                 invert={true}
