db83d3fb979d03da05e7123dd66ed389c9e82d84
hellcat29A <bhiri22@gmail.com>
2024-11-19 17:42:49 +0100

    fix:resize-images-and-generate-images-for-features-section

--- src/common/data.ts
+++ src/common/data.ts
@@ -3,4 +3,5 @@ export const swiperImages: { src: string; alt: string }[] = [
   { src: "images/hero-section/2.webp", alt: "slide 2" },
   { src: "images/hero-section/3.webp", alt: "slide 3" },
   { src: "images/hero-section/4.webp", alt: "slide 4" },
+  { src: "images/hero-section/5.webp", alt: "slide 5" },
 ];
